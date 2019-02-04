3e2a95303a223ee82f07a4d2654e4a925462e022
cocteau666 <cocteau666@gmail.com>
2019-02-04 20:00:06 +0900

    build active

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # qsplayground
 This is a tool for Q# throughout a web access written in python3 with Flask
+
+## How to use
+
+Before, python3 and Flask library.
+To install Flask, use pip.
+    $ pip install Flask
+
+To set dotnet command path, change a variable "dotnet_command_path" in dotnetqs.py.
+
+To run, do this.
+    $ LANG=c python3 dotnetqs.py
+
+To use, access from Web browser and port 5000/TCP.
+
+## Command
+
+### create new project
+    http://hostname:5000/create?ProjectName
+
+### build project
+    http://hostname:5000/build?ProjectName
+
+### Override Driver.cs
+
+### Override Operations.qs
+
--- dotnetqs.py
+++ dotnetqs.py
@@ -1,19 +1,35 @@
 # coding: utf-8
 
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 import subprocess
+import os
+
+dotnet_command_path = "/usr/local/share/dotnet/dotnet"
 
 @app.route('/create')
 def create():
-  output = subprocess.check_output(["/usr/local/share/dotnet/dotnet", "new", "console", "-lang", "Q#", "--output", "Bell"])
+  path = request.query_string
+  output = subprocess.check_output([dotnet_command_path, "new", "console", "-lang", "Q#", "--output", path])
   return output
 
 @app.route('/build')
 def build():
-  output = subprocess.check_output(["/usr/local/share/dotnet/dotnet", "run"])
+  path = request.query_string
+  os.chdir(path)
+  output = subprocess.check_output([dotnet_command_path, "run"])
+  os.chdir("../")
   return output
+
+@app.route('/editdriver')
+def editdriver():
+  return ""
+
+@app.route('/editoperations')
+def editoperations():
+  return ""
 
 if __name__ == "__main__":
   app.run()
