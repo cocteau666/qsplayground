c301b38ae3612108f6f676f240b2d45d58214a40
Takeshi SAKURAI <cocteau666@gmail.com>
2019-02-12 00:17:51 +0900

    js

--- static/js/qs.js
+++ static/js/qs.js
@@ -2,11 +2,9 @@ const request = new XMLHttpRequest();
 
 function getProjectList() {
   request.onreadystatechange = function() {
-    if (request.status == 4) {
-      if (request.status == 200) {
-        console.log(request.responseText);
-        document.getElementById('ProjectList').innerHTML = request.responseText;
-      }
+    if (request.status == 200) {
+      console.log(request.responseText);
+      document.getElementById('ProjectList').innerHTML = request.responseText;
     }
   }
   request.open("GET", "/list", false);
@@ -15,13 +13,30 @@ function getProjectList() {
 
 function createProject(projectName) {
   request.onreadystatechange = function() {
-    if (request.status == 4) {
-      if (request.status == 200) {
-        console.log(request.responseText);
-      }
+    if (request.status == 200) {
+      console.log(request.responseText);
     }
   }
   request.open("GET", `/create?${projectName}`, false);
   request.send();
 }
 
+function getDriverCode(projectName) {
+  request.onreadystatechange = function() {
+    if (request.status == 200) {
+      document.getElementById('Driver').innerHTML = request.responseText;
+    }
+  }
+  request.open("GET", `/driver?${projectName}`, false);
+  request.send();
+}
+
+function getOperationsCode(projectName) {
+  request.onreadystatechange = function() {
+    if (request.status == 200) {
+      document.getElementById('Operations').innerHTML = request.responseText;
+    }
+  }
+  request.open("GET", `/operations?${projectName}`, false);
+  request.send();
+}
